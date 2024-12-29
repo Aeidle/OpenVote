ed32ed6e97717f970f0cc4b18edd31ad85c25fec
StilinskiAnas <anas.etthabity@edu.uiz.ac.ma>
2024-12-29 21:12:58 +0100

    setup project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import type { AppProps } from 'next/app';
-import AppBar from '../components/AppBar';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <AppBar />
-      <main className="pt-16 px-4"> {/* Add padding top for AppBar and horizontal padding */}
+      <main className=""> {/* Add padding top for AppBar and horizontal padding */}
         <Component {...pageProps} />
       </main>
     </div>
